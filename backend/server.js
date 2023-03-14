be842f744bbc98b6f621692149419bff374c32da
dmuthui <vyda2002us@gmail.com>
2023-03-14 19:11:20 +0300

    Adds projectRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,65 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = express.Router();
 
 const productRoutes = require('./routes/productRoutes') //new code
 
-// Product Model
-const Product = require('./models/Product');
-// @route GET /products
-// @desc Get ALL products
-router.get('/', (req,res)=>{
-   // Fetch all products from database
-   Product.find({}, (error, products)=>{
-       if (error) console.log(error)
-       res.json(products)
-   })
-})
 
-// @route POST /products
-// @desc  Create a product
-router.post('/', (req,res)=>{
-   console.log(req.body)
-   // Create a product item
-   const newProduct = new Product({
-       name: req.body.name,
-       description: req.body.description,
-       price: req.body.price,
-       quantity: req.body.quantity,
-   });
-
-   newProduct.save((err, product)=>{
-       if (err) console.log(err)
-       res.json(product)
-   })
-})
-// @route PUT api/products/:id
-// @desc  Update a product
-router.put('/:id', (req,res)=>{
-   // Update a product in the database
-   Product.updateOne({_id:req.params.id},{
-       name: req.body.name,
-       description: req.body.description,
-       price: req.body.price,
-       quantity: req.body.quantity,
-   }, {upsert: true}, (err, product)=>{
-       if(err) console.log(err);
-       res.json(product)
-   })
-})
-// @route DELETE api/products/:id
-// @desc  Delete a product
-router.delete('/:id', (req,res)=>{
-   // Delete a product from database
-   Product.deleteOne({_id: req.params.id}, (err)=>{
-       if (err){
-           console.log(err)
-           res.json({success:false})
-       }else{
-           res.json({success:true})
-       }
-   })
-})
 // Initializing express
 const app = express()
 
@@ -83,16 +27,12 @@ db.on('error', (error)=>{
    console.log(error);
 })
 
-
-
-
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
 // Use Routes
 app.use('/products', productRoutes)  //new code
+
 app.listen(PORT, ()=>{
    console.log(`Server listening on port ${PORT}`)
 })
-
-module.exports = router;
